7032f4dd685533ef8ceae6384b1868e886e931ad
bill <tom230023@gmail.com>
2025-10-07 22:28:38 +0800

    deps: jest to vitest

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+    tsconfigPaths(),
+  ],
+});
