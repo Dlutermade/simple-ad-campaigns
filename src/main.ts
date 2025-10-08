992ff321d7ddab2bc5a36c8cb750819153ff9080
bill <tom230023@gmail.com>
2025-10-08 19:00:51 +0800

    feat: setup swag

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,14 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple Ad Campaigns')
+    .setDescription('The Simple Ad Campaigns API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app as any, config);
+  SwaggerModule.setup('api', app as any, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
